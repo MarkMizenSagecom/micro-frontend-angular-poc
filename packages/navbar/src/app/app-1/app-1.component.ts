68677facc2787d5499f08e9ca643b8fa9d237478
Mark Mizen <mark.c.mizen@gmail.com>
2020-10-05 20:55:45 +0100

    Update to app components

--- packages/navbar/src/app/app-1/app-1.component.ts
+++ packages/navbar/src/app/app-1/app-1.component.ts
@@ -2,10 +2,6 @@ import { Component } from "@angular/core";
 
 @Component({
   'selector': 'app-1-test',
-  template: `
-  <div>
-    This shows on app 1
-  </div>
-  `
+  template: ''
 })
 export class App1Component {}
--- packages/navbar/src/app/empty-route/empty-route.component.ts
+++ packages/navbar/src/app/empty-route/empty-route.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'navbar-empty-route',
-  template: 'This is a test...',
+  template: '',
 })
 export class EmptyRouteComponent {
 }
